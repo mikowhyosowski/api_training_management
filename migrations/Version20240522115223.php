4ff24b63e00ea7fd378a71dd4bde65f0c207f7b2
Mikołaj Osowski <mikolaj.osowski@gmail.com>
2024-05-22 15:42:56 +0200

    Add required dev bundles

--- migrations/Version20240522115223.php
+++ migrations/Version20240522115223.php
@@ -33,13 +33,13 @@ final class Version20240522115223 extends AbstractMigration
             id INT AUTO_INCREMENT NOT NULL,
             name VARCHAR(512) NOT NULL,
             instructor_id INT NOT NULL,
-            date_time DATETIME NOT NULL,
+            training_datetime DATETIME NOT NULL,
             price DECIMAL NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             PRIMARY KEY(id),
             UNIQUE INDEX UNIQ_name (name),
-            INDEX IDX_date_time (date_time),
+            INDEX IDX_training_datetime (training_datetime),
             CONSTRAINT FK_instructor_id FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE
         ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
